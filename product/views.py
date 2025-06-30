51b5784bf78e83d308904c0105332d59b5ecf5f7
ReptiloidAnunakTestgit config --global user.email stepanovartemijdev@gmail.comgit config user.name ReptiloidAnunakTestgit config user.email stepanovartemijdev@gmail.comgit config --global user.name ReptiloidAnunak <amanitocornholio@proton.me>
2025-06-30 19:18:55 -0300

    READY: sort table

--- product/views.py
+++ product/views.py
@@ -1,6 +1,4 @@
-
 from django.shortcuts import render
-
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
@@ -15,24 +13,16 @@ from parsers.wb_parser import get_wb_products
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
-
-
 log_api.info('🌐 API Server is running ')
-
 log_api.info(f"[LOGGER] LOGS_API = {LOGS_API!r}")
 log_api.info(f"[LOGGER] LOGS_PARSER = {LOGS_PARSER!r}")
-
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class ParseProduct(APIView):
     def get(self, request):
 
-        global category
-        category = 'Сделайте запрос'
-
         log_api.info(f"GET - products page")
-        
         log_api.info(f"GET QUERY PARAMS: {request.GET.dict()}")
 
         category = request.GET.get('category')
@@ -40,46 +30,82 @@ class ParseProduct(APIView):
         price_max = request.GET.get('price_max')
         rating = request.GET.get('rating')
         review = request.GET.get('review')
+        
 
         log_api.info(f"GET - {category} {price_min} {price_max} {rating} {review}")
 
-        global products
         products = Product.objects.all()
-        
         if price_min:
-            price_min = float(price_min)
-            products = [prod for prod in products if prod.price >= price_min]
+            try:
+                products = products.filter(price__gte=float(price_min))
+            except ValueError:
+                pass
 
         if price_max:
-            price_max = float(price_max)
-            products = [prod for prod in products if prod.price <= price_max]
-        
+            try:
+                products = products.filter(price__lte=float(price_max))
+            except ValueError:
+                pass
+
         if rating:
-            rating = float(rating)
-            products = [prod for prod in products if prod.rating >= rating]
+            try:
+                products = products.filter(rating__gte=float(rating))
+            except ValueError:
+                pass
 
         if review:
-            review = int(review)
-            products = [prod for prod in products if prod.rating >= review]
+            try:
+                products = products.filter(review_amount__gte=int(review))
+            except ValueError:
+                pass
+        
+        sort_mapping = {
+            'review': 'review_amount',
+            'rating': 'rating',
+            'price': 'price',
+            'price_original': 'price_original'
+        }
 
-        return render(request, 'products_page.html', {'products': products, 
-                                                      'category': category, 
-                                                      'price_min': price_min, 
-                                                      'price_max': price_max, 
-                                                      'rating': rating, 
-                                                      'review': review
-                                                      })
-    
+        sort = request.GET.get('sort')
+        actual_sort_field = None
+
+        if sort:
+            reverse = sort.startswith('-')
+            clean_sort = sort.lstrip('-')
+
+            if clean_sort in sort_mapping:
+                actual_sort_field = sort_mapping[clean_sort]
+                if reverse:
+                    actual_sort_field = '-' + actual_sort_field
+
+
+        if actual_sort_field:
+            products = products.order_by(actual_sort_field)
+
+        # actual_sort_field
+
+
+        return render(request, 'products_page.html', {
+            'products': products,
+            'category': category,
+            'price_min': price_min,
+            'price_max': price_max,
+            'rating': rating,
+            'review': review,
+            'sort': sort
+        })
+
     def post(self, request):
         Product.objects.all().delete()
 
-        category = request.data.get('category_name')
-        price_min = request.data.get('price_min')
-        price_max = request.data.get('price_max')
-        rating = request.data.get('rating')
-        review = request.data.get('review')
-        
-        log_api.info(f"POST:\n\nproducts_category: {category}, price_min: {price_min}, price_max: {price_max} rating: {rating} review: {review}\n\n")
+        category = request.data.get('category_name', '')
+        price_min = request.data.get('price_min', '')
+        price_max = request.data.get('price_max', '')
+        rating = request.data.get('rating', '')
+        review = request.data.get('review', '')
+        sort = request.data.get('sort', '')
+
+        log_api.info(f"POST:\n\nproducts_category: {category}, price_min: {price_min}, price_max: {price_max} rating: {rating} review: {review} sort: {sort}\n\n")
 
         parsed_prods_json = get_wb_products(category)
         try:
@@ -87,9 +113,9 @@ class ParseProduct(APIView):
         except KeyError:
             prods_json_lst = []
             log_api.error('Restrictions from Wildberries server. Try again')
-        
+
         log_api.info(f"Len: {len(prods_json_lst)}")
-        
+
         response_dict = {"Widlberries parsing products": None}
 
         ok_data_msg = '🟢 Products data has been received'
@@ -102,34 +128,36 @@ class ParseProduct(APIView):
             log_api.info(no_data_msg)
             response_dict['Widlberries parsing products'] = no_data_msg
             return Response(response_dict, status=status.HTTP_200_OK)
-        
+
         for prod in prods_json_lst:
-            product = Product(id=prod['id'],
-                              title=prod["name"],
-                              price_original=prod["priceU"] / 100,
-                              price=prod["salePriceU"] / 100,
-                              rating=prod["rating"],
-                              review_amount=prod["feedbacks"],
-                              )
+            product = Product(
+                id=prod['id'],
+                title=prod["name"],
+                price_original=prod["priceU"] / 100,
+                price=prod["salePriceU"] / 100,
+                rating=prod["rating"],
+                review_amount=prod["feedbacks"],
+            )
             product.save()
 
-        global products
-        products = Product.objects.all()
+        valid_sort_fields = {'price_original', 'price', 'rating', 'review'}
 
-        params = {}
-        if category:
-            params['category'] = category
-        if price_min:
-            params['price_min'] = price_min
-        if price_max:
-            params['price_max'] = price_max
-        if rating:
-            params['rating'] = rating
-        if review:
-            params['review'] = review
+        if sort:
+            sort_field = sort.lstrip('-')
+            if sort_field in valid_sort_fields:
+                products = products.order_by(sort)
 
-
+        params = {
+            'category': category,
+            'price_min': price_min,
+            'price_max': price_max,
+            'rating': rating,
+            'review': review,
+            'sort': sort
+        }
+        
         query_string = '&'.join([f'{k}={v}' for k, v in params.items()])
+
         url = reverse('products-list')
         if query_string:
             url += '?' + query_string
